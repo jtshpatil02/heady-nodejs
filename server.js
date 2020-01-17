1e6be3c1fdf45bbb64945f2fdf431cd0ae02873b
jtshpatil02 <57857794+jtshpatil02@users.noreply.github.com>
2020-01-17 19:02:49 +0530

    changes in loadbalancer

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const http = require('http'),
 
 server.on('request',(request,response)=>{
    response.writeHead(200,{'Content-Type':'text/plain'});
-   response.write('Hello world');
+   response.write(' Hello world');
+      response.write(' Container Loadbalancing Done Here.');
       response.write('Made By Jitesh Patil');
    response.end();
 });
